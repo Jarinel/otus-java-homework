321dfa1ebd4788a950eb7126d4bc9794f59480dc
jarinel <geo.tsarkov@gmail.com>
2022-06-15 22:10:46 +0300

    hw02-generics: Done

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,31 @@
 package homework;
 
 
+import java.util.AbstractMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
-
-    //todo: 3. надо реализовать методы этого класса
-    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final TreeMap<Customer, String> customerToStringMap = new TreeMap<>(
+            Comparator.comparing(Customer::getScores)
+    );
 
     public Map.Entry<Customer, String> getSmallest() {
-        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        return copyEntry(customerToStringMap.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        return copyEntry(customerToStringMap.higherEntry(customer));
     }
 
     public void add(Customer customer, String data) {
+        customerToStringMap.put(customer, data);
+    }
 
+    private Map.Entry<Customer, String> copyEntry(Map.Entry<Customer, String> entry) {
+        return entry == null
+                ? null
+                : new AbstractMap.SimpleEntry<>(entry.getKey().copy(), entry.getValue());
     }
 }
